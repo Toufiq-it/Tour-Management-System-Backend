bd527e27917ad5f7ad66b7ce3fb31167cac03706
Toufiq-it <ittoufikit@gmail.com>
2025-08-03 01:33:51 +0600

    zod validation added

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
 import { userController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { Router } from "express";
 
 const router = Router();
 
-router.post("/register", userController.createUser);
+router.post("/register", validateRequest(createUserZodSchema), userController.createUser);
 router.get("/all-users", userController.getAllUsers);
 
-export const UserRoutes = router
+export const UserRoutes = router;
