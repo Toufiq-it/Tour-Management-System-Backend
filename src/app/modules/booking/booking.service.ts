e066b659c1603e9d265ac33fff523f61cbdd6396
Toufiq-it <ittoufikit@gmail.com>
2025-08-12 02:26:55 +0600

    SSLCommerz

--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import AppError from "../../errorHelpers/AppError";
 import { User } from "../user/user.model";
 import { BOOKING_STATUS, IBooking } from "./booking.interface";
@@ -6,6 +7,8 @@ import { Booking } from "./booking.model";
 import { Payment } from "../payment/payment.model";
 import { PAYMENT_STATUS } from "../payment/payment.interface";
 import { Tour } from "../tour/tour.model";
+import { SSLService } from "../sslCommerz/sslCommerz.service";
+import { ISSLCommerz } from "../sslCommerz/sslCommerz.interface";
 
 const getTransactionId = () => {
     return `tran_${Date.now()}_${Math.floor(Math.random() * 1000)}`
@@ -53,7 +56,7 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
 
         // Payment
         const payment = await Payment.create([{
-            // booking: booking[0]._id,
+            booking: booking[0]._id,
             status: PAYMENT_STATUS.UNPAID,
             transactionId: transactionId,
             amount: amount,
@@ -68,20 +71,47 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
             .populate("tour", "title costFrom")
             .populate("payment");
 
+        // SSL
+        const userAddress = (updatedBooking?.user as any).address
+        const userEmail = (updatedBooking?.user as any).email
+        const userPhoneNumber = (updatedBooking?.user as any).phone
+        const userName = (updatedBooking?.user as any).name
+
+        const sslPayload: ISSLCommerz = {
+            address: userAddress,
+            email: userEmail,
+            name: userName,
+            phoneNumber: userPhoneNumber,
+            amount: amount,
+            transactionId: transactionId,
+        }
+        const sslPayment = await SSLService.sslPaymentInit(sslPayload)
+        console.log(sslPayment);
+        
+
         // virtual DB -> Real DB
         await session.commitTransaction(); // transaction
         session.endSession();
 
-        return updatedBooking;
+        return {
+            paymentUrl: sslPayment.GatewayPageURL,
+            booking: updatedBooking
+        }
+
     } catch (error) {
         // virtual DB remove all process and not adding DB
         await session.abortTransaction(); // roll Back
         session.endSession();
         throw error;
     }
-
-
 };
+
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Complete -> Backend(localhost:5000/api/v1/payment/success) -> Update Payment(PAID) & Booking(CONFIRM) -> redirect to frontend -> Frontend(localhost:5173/payment/success)
+
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Fail / Cancel -> Backend(localhost:5000) -> Update Payment(FAIL / CANCEL) & Booking(FAIL / CANCEL) -> redirect to frontend -> Frontend(localhost:5173/payment/cancel or fail)
+
+
+
 const getUserBookings = async () => {
 
     return {}
